f5447522bdb7049272880ccbef1dfdd484e34839
GABRIEL DE ESPÍNDOLA MARTINS <gabriel_e_martins@estudante.sesisenai.org.br>
2025-03-05 12:42:04 -0300

    primeiroCommit

---
+++ verificador_cpf.js
@@ -0,0 +1,19 @@
+
+
+// separar cada numero do cpf em um item do array (replace, split)
+// criar uma variavel chamada somatorio
+// criar loop para multiplicar os digitos do cpf e somar na variavel somatorio
+// calcular o resto da divisao por 11 da variavel somatorio 
+// criar um condicionalpara verificar se o digito é valido
+
+// criar uma variavel para armazenar o cpf
+let cpf = "12345678900"
+
+    if (!isNaN(cpf)){
+       cpf = Number(cpf)
+    }
+
+// verificar se a variavel tem 11 digitos
+    
+    
+    
