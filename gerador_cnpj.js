5ab551e018764f8218e09e500682573f7fd3ccf2
GABRIEL DE ESPÍNDOLA MARTINS <gabriel_e_martins@estudante.sesisenai.org.br>
2025-03-13 11:21:06 -0300

    final

--- gerador_cnpj.js
+++ gerador_cnpj.js
@@ -1,8 +1,37 @@
-Math.random() * (9 - 0) + 0;
+//gerar 8 numeros aleatorios
+// adicionar o 0001 ao final dos 8 digitos
+// gerar 13 digito = multiplicar pelo array 543298765432
+// somar tudo, vereficar se resto é 2
+// se for 0 ou 1 = 0, else é numero -11
 
-let aleatorio = " "
-for(let i =0; i < 8; i++){
-    let num = (Math.random() * (9 - 0) + 0).toString
-    aleatorio += num
+function gerarCNPJ(){
 
+let array = []
+for (let i = 0; i < 8; i++){
+    array.push(Math.floor(Math.random() * 10))
 }
+array.push(0, 0, 0, 1)
+
+
+function calcularVerificador(cnpj, pesos){
+    let soma = 0
+    for (let i = 0; i< pesos.length; i++){
+        soma += cnpj[i] * pesos[i]
+    }
+    let resto = soma % 11
+    return resto < 2 ? 0 : 11 - resto 
+}
+let peso1 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+let peso2 = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+
+const primeiroDigito = calcularVerificador(array, peso1)
+array.push(primeiroDigito)
+
+const segundoDigito = calcularVerificador(array, peso2)
+array.push(segundoDigito)
+
+return array 
+    .join('')
+    .replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, '$1.$2.$3.$4-$5')
+}
+console.log(gerarCNPJ())
